db2de7398de8e58c43db7446a8db6050d3d30f91
László Vaskó <laszlo.vasko@outlook.com>
2021-10-10 22:28:23 +0200

    overlay: use `emacsGcc` as it is more up to date

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,7 +4,7 @@ final: prev:
 let
   eo = inputs.emacs-overlay.overlay final prev;
   customEmacsPackages = final.callPackage ./customEmacsPackages.nix { inherit inputs; };
-  emacs = final.emacsPgtkGcc;
+  emacs = final.emacsGcc;
   parse = final.callPackage "${inputs.emacs-overlay}/parse.nix" { };
 in
 eo // {
