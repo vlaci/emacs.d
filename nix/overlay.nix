1587e971bb8098fd898cc653dec417d6ec8c2e59
László Vaskó <1771332+vlaci@users.noreply.github.com>
2021-12-30 13:53:01 +0100

    overlay: Use master version
    `emacsGcc` is using the unstable `28.0` version which currently has a
    serious performance regression while using LSP

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,7 +4,7 @@ final: prev:
 let
   eo = inputs.emacs-overlay.overlay final prev;
   customEmacsPackages = final.callPackage ./customEmacsPackages.nix { inherit inputs; };
-  emacs = final.emacsGcc;
+  emacs = final.emacsPgtkGcc;
   parse = final.callPackage "${inputs.emacs-overlay}/parse.nix" { };
 in
 eo // {
