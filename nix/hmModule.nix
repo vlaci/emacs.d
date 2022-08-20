12f660f516293d09f99fa58d98556d0d3f910abd
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-08-20 19:17:43 +0200

    home-manager: improved lisp form support

--- nix/hmModule.nix
+++ nix/hmModule.nix
@@ -10,20 +10,30 @@ let
     if isBool value then
       (if value then "t" else "nil")
     else if isInt value then
-      value
+      toString value
     else if isList value then
-      '''(${concatMapStringsSep " " toEmacsLisp value})''
+      let
+        car = builtins.head value;
+        tail = concatMapStringsSep " " toEmacsLisp (builtins.tail value);
+        symbols = {
+          list = "(list ${tail})";
+          quote = "'(${tail})";
+          backquote = "`(${tail}})";
+          cons = "(${toEmacsLisp (builtins.elemAt value 1)} . ${toEmacsLisp (builtins.elemAt value 2)})";
+        };
+      in
+        symbols.${car} or "(${car}\n${tail})"
     else if isAttrs value && value ? elisp then
       value.elisp
+    else if isAttrs value then
+      generators.toKeyValue
+        {
+          mkKeyValue = key: value: "  ${key} ${toEmacsLisp value}";
+        }
+        value
     else
       ''"${toString value}"'';
-  toEmacsConfig = generators.toKeyValue {
-    mkKeyValue = key: value:
-      let
-        value' = toEmacsLisp value;
-      in
-      "(setq ${key} ${value'})";
-  };
+  toEmacsConfig = settings: toEmacsLisp [ "+set-defaults!" settings ];
 in
 {
   options.emacsVlaci = {
