9942285e502cefb51376d2d0cb31d89aa09df57b
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-04-01 16:23:09 +0200

    additional tree-sitter modules

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,7 +4,43 @@ final: prev:
 let
   eo = inputs.emacs-overlay.overlay final prev;
   customEmacsPackages = final.callPackage ./customEmacsPackages.nix { inherit inputs; };
-  emacs = final.emacsPgtk;
+  emacs = (final.emacsPgtk.override {
+    treeSitterPlugins = with final.tree-sitter-grammars; [
+      tree-sitter-bash
+      tree-sitter-beancount
+      tree-sitter-c
+      tree-sitter-c-sharp
+      tree-sitter-cmake
+      tree-sitter-cpp
+      tree-sitter-css
+      tree-sitter-dockerfile
+      tree-sitter-elisp
+      tree-sitter-go
+      tree-sitter-gomod
+      tree-sitter-graphql
+      tree-sitter-haskell
+      tree-sitter-html
+      tree-sitter-http
+      tree-sitter-java
+      tree-sitter-javascript
+      tree-sitter-json
+      tree-sitter-julia
+      tree-sitter-lua
+      tree-sitter-make
+      tree-sitter-nix
+      tree-sitter-ocaml
+      tree-sitter-python
+      tree-sitter-regex
+      tree-sitter-ruby
+      tree-sitter-rust
+      tree-sitter-scala
+      tree-sitter-toml
+      tree-sitter-tsx
+      tree-sitter-typescript
+      tree-sitter-yaml
+      tree-sitter-zig
+    ];
+  });
   fromElisp = (import "${inputs.fromElisp}") { pkgs = final; };
 in
 eo // fromElisp // {
